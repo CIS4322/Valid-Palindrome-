0ea49cfc47ea8e9bc07b0f2f680af02a6d0a56ad
JoshAoki <73375352+JoshAoki@users.noreply.github.com>
2022-02-21 14:45:25 -0600

    first commit

---
+++ ValidPalindrome/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ValidPalindrome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.palindromeList = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(36, 26);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // palindromeList
+            // 
+            this.palindromeList.HideSelection = false;
+            this.palindromeList.Location = new System.Drawing.Point(36, 55);
+            this.palindromeList.Name = "palindromeList";
+            this.palindromeList.Size = new System.Drawing.Size(195, 330);
+            this.palindromeList.TabIndex = 1;
+            this.palindromeList.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.palindromeList);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ListView palindromeList;
+    }
+}
+
---
+++ ValidPalindrome/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ValidPalindrome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string data = null;
+            try
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    data = File.ReadAllText(filePath);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            string[] words = data.Split('\r', '\n');
+
+            foreach(string word in words)
+            {
+                if(!string.IsNullOrEmpty(word))
+                {
+                    if (isPalindrome(word))
+                    {
+                        palindromeList.Items.Add(new ListViewItem(new string[] { word, "true" }));
+                    }
+                    else
+                    {
+                        palindromeList.Items.Add(new ListViewItem(new string[] { word, "false" }));
+
+                    }
+                }
+                
+            }
+        }
+        public bool isPalindrome(string word)
+        {
+            int a = 0;
+            int b = word.Length - 1;
+            while (a < b)
+            {
+                char left = word[a];
+                char right = word[b];
+                if (!Char.IsLetterOrDigit(left))
+                {
+                    a++;
+                    continue;
+                }
+                if (!Char.IsLetterOrDigit(right))
+                {
+                    b--; ;
+                    continue;
+                }
+                if (char.ToLower(left) != char.ToLower(right))
+                {
+                    return false;
+                }
+                a++;
+                b--;
+            }
+            return true;            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            palindromeList.Items.Clear();
+            palindromeList.Columns.Add("Word:", 100);
+            palindromeList.Columns.Add("isPalindrome:", 120);
+            palindromeList.View = View.Details;
+            palindromeList.GridLines = true;
+            palindromeList.Sorting = SortOrder.Ascending;
+        }
+    }
+}
